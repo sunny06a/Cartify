3ccc5e95c47b6446a99256d7eca17573b06ce699
suny06a <sunny.tomar.ug20@nsut.ac.in>
2023-09-18 09:24:24 +0530

    project setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React from 'react';
 import './App.css';
 import  Header  from './component/layout/Header';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import webFont from 'webfontloader';
+
 
 function App() {
+  React.useEffect(() => {
+    webFont.load({
+      google: {
+        families: ['Roboto', 'sans-serif']
+      }
+    });
+  }, []);
+  
   return (
     <>
-    
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Header></Header>} />
+    </Routes>
+    </BrowserRouter>
     </>
   );
 }
